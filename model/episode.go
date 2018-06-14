ce0a4f8beaa6515c5baa16669b470bf4c96baee6
Anthony Massicci <anthony.massicci@outlook.com>
2018-06-14 16:25:18 -0400

    more work done on domain logic

--- model/episode.go
+++ model/episode.go
@@ -10,4 +10,41 @@ type Episode struct {
 	guid        string
 	played      bool
 	length      int
+	url         string
+}
+
+func (episode Episode) Title() string {
+	return episode.title
+}
+
+func (episode Episode) Description() string {
+	return episode.description
+}
+
+func (episode Episode) Published() *time.Time {
+	return &episode.published
+}
+
+func (episode Episode) GUID() string {
+	return episode.guid
+}
+
+func (episode Episode) Played() bool {
+	return episode.played
+}
+
+func (episode *Episode) SetPlayed() {
+	episode.played = true
+}
+
+func (episode *Episode) SetUnplayed() {
+	episode.played = false
+}
+
+func (episode Episode) Length() int {
+	return episode.length
+}
+
+func (episode Episode) URL() string {
+	return episode.url
 }
--- model/feed.go
+++ model/feed.go
@@ -14,3 +14,59 @@ type Feed struct {
 	updated, feedUpdated time.Time
 	episodes             []Episode
 }
+
+// Title: returns title of feed
+func (feed Feed) Title() string {
+	return feed.title
+}
+
+// returns description of feed
+func (feed Feed) Description() string {
+	return feed.description
+}
+
+func (feed Feed) Icon() string {
+	return feed.iconURL
+}
+
+func (feed Feed) URL() string {
+	return feed.feedURL
+}
+
+func (feed Feed) Author() string {
+	return feed.author
+}
+
+func (feed Feed) GetUpdated() *time.Time {
+	return &feed.updated
+}
+
+func (feed *Feed) SetUpdated(newTime time.Time) {
+	feed.updated = newTime
+}
+
+func (feed *Feed) SetFeedUpdated(newTime time.Time) {
+	feed.feedUpdated = newTime
+}
+
+func (feed Feed) Episodes() []Episode {
+	return feed.episodes
+}
+
+func (feed *Feed) AddEpisodes(episodes ...Episode) {
+	feed.episodes = append(episodes, feed.episodes...)
+}
+
+func (feed Feed) UnplayedEpisodes() []Episode {
+	episodes := make([]Episode, 0)
+	for _, episode := range feed.episodes {
+		if !episode.played {
+			episodes = append(episodes, episode)
+		}
+	}
+	return episodes
+}
+
+func (feed Feed) NumEpisodesUnplayed() int {
+	return len(feed.UnplayedEpisodes())
+}
